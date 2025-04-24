34e051c2d8045419ffcabec48e447d4192a97b2b
Raitis Jermacāns <raitis.jermacans@reatom.com>
2025-04-24 16:58:25 +0300

    some changew

--- backend/app/Http/Controllers/ResultController.php
+++ backend/app/Http/Controllers/ResultController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\GameResults;
+use App\Models\PlayerResults;
 
 class ResultController extends Controller
 {
@@ -30,5 +31,28 @@ class ResultController extends Controller
         $gameResult->save();
 
         return response()->json($gameResult);
+    }
+    public function createPlayerResult(Request $request)
+    {
+        // Validate the request data
+        $request->validate([
+            'game_result_id' => 'required',
+            'player_id' => 'required',
+            'kills' => 'required',
+            'deaths' => 'required|integer',
+            'assists' => 'required|integer',
+        ]);
+
+        // Create a new game result
+        $playerResult = new PlayerResults();
+        $playerResult->game_result_id = $request->game_result_id;
+        $playerResult->player_id = $request->player_id;
+        $playerResult->kills = $request->kills;
+        $playerResult->deaths = $request->deaths;
+        $playerResult->assists = $request->assists;
+        $playerResult->points = ($request->kills * 3) - ($request->deaths * 2) + $request->assists;
+        $playerResult->save();
+
+        return response()->json($playerResult);
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\TeamController;
 use App\Http\Controllers\ResultController;
 use App\Http\Controllers\TournamentController;
 use App\Http\Controllers\Auth\RegisteredUserController;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -28,3 +29,25 @@ Route::get('/getTournamentInfo/{tournamentId}', [TournamentController::class, 'g
 
 //results
 Route::post('/createGameResult', [ResultController::class, 'createGameResult']);
+Route::post('/createPlayerResult', [ResultController::class, 'createPlayerResult']);
+Route::post('/login', function (Request $request) {
+    $request->validate([
+        'email' => 'required|email',
+        'password' => 'required',
+    ]);
+
+    $user = User::where('email', $request->email)->first();
+
+    if (! $user || ! Hash::check($request->password, $user->password)) {
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    }
+
+    return response()->json([
+        'token' => $user->createToken('api-token')->plainTextToken,
+    ]);
+});
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//fantasy teams
